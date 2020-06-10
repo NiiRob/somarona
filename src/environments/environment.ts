27e5543a8414584d1ae653b0d38e31d0019af7e1
Robert <radjeilaryea@gmail.com>
2020-06-10 22:03:33 +0000

    Version 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireConfig: {
+    apiKey: 'AIzaSyBBEPdJErSYTN42Vcg1e7s1szj3vnbOJWA',
+    authDomain: 'ronatrack-957c0.firebaseapp.com',
+    databaseURL: 'https://ronatrack-957c0.firebaseio.com',
+    projectId: 'ronatrack-957c0',
+    storageBucket: 'ronatrack-957c0.appspot.com',
+    messagingSenderId: '966343968495',
+    appId: '1:966343968495:web:2c10acde9c8e6c96b43d1a',
+    measurementId: 'G-80GW4Q1HSL'
+  },
+  coronaApi: 'https://api.covid19api.com',
+  ipapi: 'https://ipapi.co',
+  restApi: 'https://restcountries.eu/rest/v2/all?fields=name;flag',
+  gapi: 'AIzaSyB-97njo5JCAj6Ju468pWI5IygpuAGzBW0'
 };
 
 /*
